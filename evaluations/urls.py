4f396722669e9d31e357828cf503a247c8600df1
DADIGA-AKSHAYA-2201 <akshayadadiga@gmail.com>
2024-11-06 21:27:57 -0600

    Added Faculty
    Added CRUD operations for Faculty

--- evaluations/urls.py
+++ evaluations/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('add-student/', views.addStudent, name='add_student'),
     path('delete-student/<str:student_id>/', views.delete_student, name='delete_student'),
     path('update-student/<str:student_id>/', views.update_student, name='update_student'),
-    
+    path('faculties/', views.faculty_list, name='faculty_list'),
+    path('faculties/add/', views.add_faculty, name='add_faculty'),
+    path('faculties/update/<str:pk>/', views.update_faculty, name='update_faculty'),
+    path('faculties/delete/<str:pk>/', views.delete_faculty, name='delete_faculty'),
+
 ]
--- evaluations/views.py
+++ evaluations/views.py
@@ -37,3 +37,36 @@ def update_student(request, student_id):
     else:
         form = StudentForm(instance=student)
     return render(request, 'update_student.html', {'form': form, 'student': student})
+def faculty_list(request):
+    all_faculties = Faculty.objects.all()
+    context = {'faculties': all_faculties}
+    # Render the template with the context
+    return render(request, 'faculty_list.html', context)
+
+def add_faculty(request):
+    if request.method == 'POST':
+        form = FacultyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('faculty_list')
+    else:
+        form = FacultyForm()
+    return render(request, 'add_faculty.html', {'form': form})
+
+def update_faculty(request, pk):
+    faculty = get_object_or_404(Faculty, pk=pk)
+    if request.method == 'POST':
+        form = FacultyForm(request.POST, instance=faculty)
+        if form.is_valid():
+            form.save()
+            return redirect('faculty_list')
+    else:
+        form = FacultyForm(instance=faculty)
+    return render(request, 'update_faculty.html', {'form': form, 'faculty': faculty})
+
+def delete_faculty(request, pk):
+    faculty = get_object_or_404(Faculty, pk=pk)
+    if request.method == 'POST':
+        faculty.delete()
+        return redirect('faculty_list')
+    return render(request, 'delete_faculty.html', {'faculty': faculty})
